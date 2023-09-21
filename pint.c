82ad0662d81ff6829215034494daafe938f5aaaa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-21 23:18:25 +0300

    first commit (A.C)

--- pint.c
+++ pint.c
@@ -5,3 +5,16 @@ void pint(stack_t **h, unsigned int line_number)
 
 	printf("%i\n", (*h)->n);
 }
+
+int pint_error(int line_number)
+{
+	fprintf(stderr, "L%i: can't pint, stack empty\n", line_number);
+	if (vars_aircraft.stack != NULL)
+		free_stack_t(vars_aircraft.stack);
+
+	free(vars_aircraft.input);
+	free_grid(vars_aircraft.input_splitted);
+	fclose(vars_aircraft.file);
+
+	exit(EXIT_FAILURE);
+}
