e19c47972bdbf3ce860e808865ec7a9b17851eb9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-22 03:39:47 +0300

    6. sub  (A.C)

--- main.c
+++ main.c
@@ -56,14 +56,24 @@ int main(int argc, char **argv)
  */
 void compare_fun(char **input_splitted, const int line_number, stack_t **stack)
 {
-	instruction_t functions[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub}, {"div", divide}, {"mul", mul}};
+	instruction_t functions[] =
+		{
+			{"push", push},
+			{"pall", pall},
+			{"pint", pint},
+			{"pop", pop},
+			{"swap", swap},
+			{"add", add},
+			{"nop", nop},
+			{"sub", sub},
+			{"div", divide},
+			{"mul", mul}};
 	int i;
 
 	if (double_arr_len(input_splitted) == 0)
 		return;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 
 		if (strcmp(functions[i].opcode, input_splitted[0]) == 0)
