3d7580e6eb76165d850bf19306697a089eda43d6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-24 05:27:22 +0300

    first commit (A.C)

--- rotr.c
+++ rotr.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
 #include "monty.h"
+char *int_toString(int num);
+int _pow(int x, int y);
 int delete_last_node(stack_t **h, int *n);
 /**
  * rotr - prints the string starting at the top of the stack,
@@ -16,6 +21,7 @@ int delete_last_node(stack_t **h, int *n);
 void rotr(stack_t **h, unsigned int line_number)
 {
 	int tmp;
+	char *str_tmp;
 
 	UNUSED(line_number);
 
@@ -23,7 +29,12 @@ void rotr(stack_t **h, unsigned int line_number)
 		return;
 
 	delete_last_node(h, &tmp);
-	push(h, tmp);
+
+	str_tmp = int_toString(tmp);
+
+	add_at_beginning_stack_t(h, tmp);
+
+	free(str_tmp);
 }
 /**
  * delete_last_node  - function that delete the last node
@@ -55,4 +66,59 @@ int delete_last_node(stack_t **h, int *n)
 		}
 	}
 	return (0);
+}
+char *int_toString(int num)
+{
+	char *str = malloc(100 * sizeof(char));
+	int num_cpy = num, i, j = 0;
+	int number_len = 0;
+
+	if (num < 0)
+	{
+		num = -(num);
+		num_cpy = num;
+		str[0] = '-';
+		j = 1;
+	}
+
+	while (num_cpy > 0)
+	{
+		number_len++;
+		num_cpy /= 10;
+	}
+
+	for (i = number_len - 1; i >= 0; i--, j++)
+	{
+
+		int tmp2 = ((int)(num / _pow(10, i)) % 10);
+
+		str[j] = tmp2 + 48;
+		if (i == 0)
+		{
+			j++;
+			break;
+		}
+	}
+	str[j] = '\0';
+	return str;
+}
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y
+ * then return it.
+ *
+ * @x: parm of The base value that wanted to be calcualted.
+ * @y:parm of The exponent value that wanted to be raised.
+ *
+ * Return: The value of x raised to the power of y,
+ * or -1 if y is negative.
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	if (y > 0 && y != 1)
+		return (x * _pow(x, y - 1));
+	return (x);
 }
